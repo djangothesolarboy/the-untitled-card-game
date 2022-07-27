7301ca280490da3c322106f126c5c41fad10cb8c
morrantho <morrantho@gmail.com>
2022-07-26 21:19:30 -0400

    splash done

--- src/states/state_splash.js
+++ src/states/state_splash.js
@@ -1,33 +1,74 @@
+/*
+	Note:
+
+	Set state to splash again if you want multiple different promos.
+	Put logos / brands in an array of strings, use an index to select the logo.
+	When you run out of indices, set state to menu.
+*/
+
 state_splash=
 {
-	max_time:10000,
+	max_time:5000,
 	current:0,
 	elapsed:0,
+	logo:undefined,
+	alpha:0,
+	text:"Baby Shit Games",
 
 	init:()=>
 	{
 		state_splash.current=time.current;
-		console.log("Splash Init",state_splash.current);
+		state_splash.logo=new Image();
+		state_splash.logo.src="./res/img/logo.jpg";
 	},
 
 	deinit:()=>
 	{
-		console.log("Splash Deinit",state_splash.current);
+
 	},
 
 	update:()=>
 	{
 		/* Call deinit after X seconds to transition to menu. */
-		let t=time.current-state_splash.current;
-		if(t>=state_splash.max_time)
+		let elapsed=time.current-state_splash.current;
+		let alpha=elapsed/state_splash.max_time;
+		
+		if(alpha>0.5)
 		{
-			state_set("menu",state_menu);
+			state_splash.alpha=-(((elapsed-state_splash.max_time)/state_splash.max_time)*2);
+		}
+		else
+		{
+			state_splash.alpha=(alpha*2);
+		}
+
+		if(elapsed>=state_splash.max_time)
+		{
+			state_set("splash");
 		}
 	},
-
+	
 	draw:()=>
 	{
-		/* Draw the company logo here. */
-		/* Indicate that a key can be pressed to skip to menu. */
+		gfx.context.globalAlpha=state_splash.alpha;
+
+		let sw=gfx.context.canvas.width;
+		let sh=gfx.context.canvas.height;
+		let lw=state_splash.logo.width;
+		let lh=state_splash.logo.height;
+		let lx=(sw/2)-(lw/2);
+		let ly=(sh/2)-(lh/2);
+
+		gfx.context.drawImage(state_splash.logo,lx,ly);
+
+		let txtw=gfx.context.measureText(state_splash.text).width;
+		gfx.context.textAlign="left";
+		gfx.context.fillStyle="#fff";
+		gfx.context.font="1rem roboto";
+
+		x=(sw/2)-(txtw);
+		y=sh-32;
+
+		gfx.context.fillText(state_splash.text,x,y);
 	},
 };
